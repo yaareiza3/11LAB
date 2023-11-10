9b0f1df3df3c46316cc4787e9f66e43f75e30f8f
yaareiza3 <150453139+yaareiza3@users.noreply.github.com>
2023-11-10 09:56:56 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2,3 +2,7 @@ import streamlit as st
 
 
 st.title ("Mi primera app")
+
+st.header("En este espacio comienzo a desarrollar mis aplicaciones para interfaces multimodales.")
+st.write("Facilmente puedo realizar backend y frontend.")
+image = Image.open('Interfaces Mult2.png')
